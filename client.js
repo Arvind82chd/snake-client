c915555e91283c3805fc924fc5d9cf194c67b743
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-03 22:28:34 +0000

    client communication updated

--- client.js
+++ client.js
@@ -11,10 +11,17 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log("Hi this is Arvind.")
+    console.log("Successfully connected to game server.")
     // code that does something when the connection is first established
   });
-
+  //conn.on("connect", () => {
+    //console.log("Name:AS")
+    // code that does something when the connection is first established
+  //});
+//process.stdin.on('data', function(message) { //sending message to server
+conn.on('connect', () => {
+    conn.write("Name:AS");
+});
   conn.on('data', function(message) {
       console.log(`Server says:  ${message}`);
   });
