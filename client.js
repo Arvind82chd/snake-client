36c4d7c627932bc63237e7cfe9cb4ce6c361d79c
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-03 23:41:39 +0000

    User keybord interface added in play.js.

--- client.js
+++ client.js
@@ -14,29 +14,14 @@ const connect = function () {
     console.log("Successfully connected to game server.")
     // code that does something when the connection is first established
   });
-  //conn.on("connect", () => {
-    //console.log("Name:AS")
-    // code that does something when the connection is first established
-  //});
-//process.stdin.on('data', function(message) { //sending message to server
+  
 conn.on('connect', () => {
     conn.write("Name:AS");
 });
 
-conn.on('connect', () => {
-    setTimeout(() => {
-        conn.write("Move: up");
-      }, 50);
-   
-});
+
 conn.on('connect', () => {
     setInterval(() => {
-        conn.write("Move: up");
-      }, 50);
-   
-});
-conn.on('connect', () => {
-    setTimeout(() => {
         conn.write("Move: up");
       }, 50);
    
--- play.js
+++ play.js
@@ -6,3 +6,28 @@ console.log("Connecting ...");
 connect();
 
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+    
+    
+  };
+
+  const handleUserInput = function () {
+    process.stdin.on('data', (key) => {
+        process.stdout.write('.');
+      });
+        if (key === '\u0003') {
+            process.exit();
+        }
+
+      console.log('after callback');
+    // your code here
+  };
+  
