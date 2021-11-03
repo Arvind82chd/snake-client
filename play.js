4d2ca67d5d2a676561a43eec3cf416e1da32d85c
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-03 23:56:13 +0000

    first load input.js.

--- play.js
+++ play.js
@@ -1,33 +1,10 @@
 const net = require("net");
-const connect = require("./client.js")
+const connect = require("./client.js");
+const { setupInput } = require("./input.js");
 
 
+setupInput();
 console.log("Connecting ...");
 connect();
 
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-    return stdin;
-    
-    
-  };
-
-  const handleUserInput = function () {
-    process.stdin.on('data', (key) => {
-        process.stdout.write('.');
-      });
-        if (key === '\u0003') {
-            process.exit();
-        }
-
-      console.log('after callback');
-    // your code here
-  };
-  
